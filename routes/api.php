349372876ba16041c29569a436cbda1453374046
jorgeedvaldo <edvaldojorge1996@gmail.com>
2025-04-06 16:23:43 +0100

    Updates

--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('/job/create', [JobController::class, 'store']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/job/create', [JobController::class, 'store']);

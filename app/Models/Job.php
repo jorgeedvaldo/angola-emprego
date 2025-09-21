7865b4bd4c1537305d2e845c66bfc65e033569e5
jorgeedvaldo <edvaldojorge1996@gmail.com>
2025-09-21 11:20:35 +0100

    Add Link and SocialMediaJob models; update Job model slug generation

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -22,17 +22,16 @@ class Job extends Model
             $job->slug = $job->generateSlug($job->title, $job->id);
             $job->save();
 
-			//SocialMediaJob::create([
-            //    'job_id' => $job->id,
-            //    'post_status' => 0,
-            //]);
+			SocialMediaJob::create([
+                'job_id' => $job->id,
+                'post_status' => 0,
+            ]);
         });
     }
 
     private function generateSlug($title, $id)
     {
         if (static::whereSlug($slug = Str::slug($title))->exists()) {
-            $max = static::whereTitle($title)->latest('id');
             $slug = $slug . '-' . $id;
         }
         return $slug;
